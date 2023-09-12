2cbbe7a093da7ab1caac171f647fb110eb3999d2
thallys roque <thallysbandeira97@gmail.com>
2023-09-12 13:07:46 -0500

    adding routes to the form and conversion function

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BinaryConverterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/converter', [BinaryConverterController::class, 'index']);
+Route::post('/converter', [BinaryConverterController::class, 'convert']);
