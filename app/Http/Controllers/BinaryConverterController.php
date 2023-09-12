4f6ea49cfeabc8a8cb12e56bb98b78d59c684885
thallys roque <thallysbandeira97@gmail.com>
2023-09-12 13:41:04 -0500

    User can enter up to 8 binary digits in an input field

--- app/Http/Controllers/BinaryConverterController.php
+++ app/Http/Controllers/BinaryConverterController.php
@@ -10,13 +10,14 @@ class BinaryConverterController extends Controller
     public function convert(Request $request)
     {
         $binaryNumber = $request->input('binary_number');
-    
-        if (!preg_match('/^[01]+$/', $binaryNumber)) {
-            return view('converter', ['error' => 'Entrada inválida. Digite apenas 0s e 1s.']);
+
+        // Verifique se a entrada é um número binário válido
+        if (!preg_match('/^[01]{1,8}$/', $binaryNumber)) {
+            return redirect('/')->with('error', 'Digite apenas 0 ou 1 em até 8 dígitos binários.');
         }
 
         $decimalNumber = bindec($binaryNumber);
-    
+
         return view('converter', compact('decimalNumber'));
     }
 }
